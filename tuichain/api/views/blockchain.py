408ba11342d2336a4d1fa62df83ffcb6a001be7f
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2021-01-07 00:09:41 +0000

    init blockchain controller

---
+++ tuichain/api/views/blockchain.py
@@ -0,0 +1,35 @@
+from rest_framework.decorators import api_view, permission_classes
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import *
+from django.contrib.auth import authenticate
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+)
+from rest_framework.response import Response
+from tuichain.api import controller
+
+
+@api_view(["GET"])
+@permission_classes((AllowAny,))
+def get_blockchain_info(request):
+    """
+    Get Blockchain information
+    """
+    if not controller:
+        return Response(
+            {"error": "Something went wrong with the Tuichain's connection..."},
+            status=HTTP_404_NOT_FOUND,
+        )
+
+    return Response(
+        {
+            "chain_id": controller.chain_id,
+            # "dai_contract_address": controller.dai_contract_address
+        },
+        status=HTTP_200_OK,
+    )
