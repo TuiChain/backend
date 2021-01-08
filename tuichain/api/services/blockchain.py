a51924dfe9a6ee1289fbc7b5fe9592288dd4ec41
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2021-01-08 00:42:20 +0000

    added test workaround

--- tuichain/api/services/blockchain.py
+++ tuichain/api/services/blockchain.py
@@ -7,11 +7,13 @@ def init():
     """
     Get the Blockchain controller
     """
-
-    return tui.Controller(
-        provider=web3.HTTPProvider(os.environ["ETHEREUM_PROVIDER"]),
-        master_account_private_key=tui.PrivateKey(
-            bytes.fromhex(os.environ["MASTER_ACCOUNT_PRIVATE_KEY"])
-        ),
-        contract_address=tui.Address(os.environ["CONTROLLER_ADDRESS"]),
-    )
+    try:
+        return tui.Controller(
+            provider=web3.HTTPProvider(os.environ["ETHEREUM_PROVIDER"]),
+            master_account_private_key=tui.PrivateKey(
+                bytes.fromhex(os.environ["MASTER_ACCOUNT_PRIVATE_KEY"])
+            ),
+            contract_address=tui.Address(os.environ["CONTROLLER_ADDRESS"]),
+        )
+    except:
+        return None
