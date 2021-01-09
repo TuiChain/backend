a61ba5afd739701a6868668ddbffa5d40b8050e6
Alberto Faria <albertofaria.dev@gmail.com>
2021-01-09 16:44:14 +0000

    Deploy test Controller if using test provider (#58)

--- tuichain/api/services/blockchain.py
+++ tuichain/api/services/blockchain.py
@@ -1,23 +1,8 @@
 from django.conf import settings
 from tuichain_ethereum import Controller
 
-
-def init():
-    """
-    Get the Blockchain controller
-    """
-
-    if (
-        settings.ETHEREUM_MASTER_ACCOUNT_PRIVATE_KEY is not None
-        and settings.ETHEREUM_CONTROLLER_ADDRESS is not None
-    ):
-
-        return Controller(
-            provider=settings.ETHEREUM_PROVIDER,
-            master_account_private_key=settings.ETHEREUM_MASTER_ACCOUNT_PRIVATE_KEY,
-            contract_address=settings.ETHEREUM_CONTROLLER_ADDRESS,
-        )
-
-    else:
-
-        return None
+controller = Controller(
+    provider=settings.ETHEREUM_PROVIDER,
+    master_account_private_key=settings.ETHEREUM_MASTER_ACCOUNT_PRIVATE_KEY,
+    contract_address=settings.ETHEREUM_CONTROLLER_ADDRESS,
+)
