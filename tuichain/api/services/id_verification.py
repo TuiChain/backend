07c1eaf25f078f880d098af001f453034f8c78cd
Nelson Sousa <36697946+nelsontss@users.noreply.github.com>
2021-01-15 18:34:59 +0000

    Kyc update, verification route (#73)
    * Update stripe request id verification, dont use return url and refresh url
    
    * Code formating
    
    * Add stripe get verification intent results route
    
    * Code format

--- tuichain/api/services/id_verification.py
+++ tuichain/api/services/id_verification.py
@@ -18,3 +18,12 @@ def create_verification_intent(id_number):
     )
 
     return response
+
+
+def get_verification_intent(verification_intent_id):
+    stripe.api_key = "sk_test_51HuGjSEXQQPxxsgPzqENPS8d8oDv3aJOGDPETgSOvLjVeB92uyDTKFcO20mFvHEHqhIObyANxPTZlabKOS2s4tk1009xlf5AxD"
+    stripe.api_version = "2020-08-27; identity_beta=v3"
+    response = stripe.stripe_object.StripeObject().request(
+        "get", f"/v1/identity/verification_intents/{verification_intent_id}", {}
+    )
+    return response
