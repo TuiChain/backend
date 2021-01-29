864f853379a1ace11f7b57b763e0494083325efc
Pedro Moreira <pedrombm19@gmail.com>
2021-01-29 14:42:32 +0000

    black

--- tuichain/api/views/investments.py
+++ tuichain/api/views/investments.py
@@ -65,8 +65,10 @@ def get_personal_investments(request, user_addr):
         student_id = (
             Loan.objects.filter(identifier=str(l.identifier)).first().student
         )
-        
-        student_name = Profile.objects.filter(user=student_id).first().user.get_full_name()
+
+        student_name = (
+            Profile.objects.filter(user=student_id).first().user.get_full_name()
+        )
 
         loan_dict = (
             Loan.objects.filter(identifier=str(l.identifier)).first().to_dict()
